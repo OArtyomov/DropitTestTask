9d31e73807a21f0825550e71d2a334337138c4a1
oleg.artyomov <oleg_artyomov@ukr.net>
2020-02-13 03:35:44 -0800

    Adjust internationalization

--- src/main/java/com/dropit/config/InternationalizationConfiguration.java
+++ src/main/java/com/dropit/config/InternationalizationConfiguration.java
@@ -3,14 +3,21 @@ package com.dropit.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import static java.util.Locale.US;
 
 @Configuration
 public class InternationalizationConfiguration {
 
-	/**
-	 * Create bean of interceptor for changing locale by a request parameter.
-	 */
+	@Bean
+	public AcceptHeaderLocaleResolver localeResolver() {
+		final AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+		acceptHeaderLocaleResolver.setDefaultLocale(US);
+		return acceptHeaderLocaleResolver;
+	}
+
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
 		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
@@ -18,10 +25,6 @@ public class InternationalizationConfiguration {
 		return lci;
 	}
 
-	/**
-	 * Create bean of message source.
-	 * Define the default locale for the locale context holder.
-	 */
 	@Bean
 	public ResourceBundleMessageSource msgSource() {
 		final ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
