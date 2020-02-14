6b8473305fc92009b16339cf166e6f7d590c99d1
oleg.artyomov <oleg_artyomov@ukr.net>
2020-02-14 07:40:29 -0800

    FIx tests

--- src/main/resources/db/migration/V2__migration.sql
+++ src/main/resources/db/migration/V2__migration.sql
@@ -2,8 +2,7 @@ CREATE TABLE package
 (
     id BIGSERIAL PRIMARY KEY,
     tag varchar(36) NOT NULL,
-    delivery_id BIGSERIAL,
-    FOREIGN KEY (delivery_id)
-        REFERENCES delivery(id)
-        ON DELETE CASCADE
+    delivery_id BIGSERIAL REFERENCES delivery(id) ON DELETE CASCADE
 );
+
+alter table package alter column delivery_id drop not null;
