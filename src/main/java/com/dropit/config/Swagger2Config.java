69b421928c11a5780a4c36d0fc57dc589186d68e
oleg.artyomov <oleg_artyomov@ukr.net>
2020-02-16 03:19:21 -0800

    Fix swagger definitions

--- src/main/java/com/dropit/config/Swagger2Config.java
+++ src/main/java/com/dropit/config/Swagger2Config.java
@@ -1,15 +1,23 @@
 package com.dropit.config;
 
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.concurrent.CompletableFuture;
+
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
 
 @Configuration
 @EnableSwagger2
@@ -18,13 +26,23 @@ public class Swagger2Config {
 	@Value("${spring.application.name}")
 	private String appName;
 
+	@Autowired
+	private TypeResolver typeResolver;
+
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors
 						.basePackage("com.dropit"))
 				.paths(PathSelectors.regex("/.*"))
-				.build().apiInfo(apiEndPointsInfo());
+				.build()
+				.genericModelSubstitutes(ResponseEntity.class)
+				.alternateTypeRules(
+						newRule(typeResolver.resolve(CompletableFuture.class,
+								typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+								typeResolver.resolve(WildcardType.class)))
+				.useDefaultResponseMessages(false)
+				.apiInfo(apiEndPointsInfo());
 	}
 
 	private ApiInfo apiEndPointsInfo() {
